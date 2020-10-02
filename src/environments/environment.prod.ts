e5452296559563554726c7698f6bc09a3e7e1857
Nyash-Mauro <mauricee423@gmail.com>
2020-10-03 01:09:08 +0300

    added apikey to environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'd8010d1f1ac61975ae43d2ad6aeffd6719218d69',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'd8010d1f1ac61975ae43d2ad6aeffd6719218d69',
 };
 
 /*
