cb0e78048b1c6a6a56143209e53634cf0632d1cf
Nyash-Mauro <mauricee423@gmail.com>
2020-10-03 05:58:24 +0300

    added html style and few logic

--- myapp/src/app/repository/repository.component.ts
+++ myapp/src/app/repository/repository.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Userservice } from '../userservice';
+import { Repo } from '../repo';
 
 @Component({
-  selector: 'app-repository',
-  templateUrl: './repository.component.html',
-  styleUrls: ['./repository.component.css']
+  selector: 'app-repositories',
+  templateUrl: './repositories.component.html',
+  styleUrls: ['./repositories.component.css'],
 })
-export class RepositoryComponent implements OnInit {
+export class RepositoriesComponent implements OnInit {
+  repo: Repo;
+  constructor(public repoService: Userservice) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  repoSearch(searchName) {
+    this.repoService.getRepos(searchName).then(
+      (results) => {
+        this.repo = this.repoService.allRepos;
+        console.log(this.repo);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  ngOnInit() {
+    this.repoSearch(' nyash-mauro ');
+  }
 }
