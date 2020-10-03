ac3ef7f6342b577cdee40df52c7c9e0237b8f1e3
Nyash-Mauro <mauricee423@gmail.com>
2020-10-03 22:09:02 +0300

    deleted the whole file as it had issues

--- myapp/src/app/cusotm.directive.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Cusotm.Directive } from './cusotm.directive';
-
-describe('Cusotm.Directive', () => {
-  it('should create an instance', () => {
-    expect(new Cusotm.Directive()).toBeTruthy();
-  });
-});
--- myapp/src/app/userservice.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Userservice } from './userservice';
-
-describe('Userservice', () => {
-  it('should create an instance', () => {
-    expect(new Userservice()).toBeTruthy();
-  });
-});
