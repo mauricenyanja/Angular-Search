8b1b7499b2ce5c9a8a5d9e2a6b786792b22ffafe
Nyash-Mauro <mauricee423@gmail.com>
2020-10-03 04:20:22 +0300

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
--- src/app/users/users.ts
+++ src/app/users/users.ts
@@ -1,23 +1,2 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http'
-
-@Injectable()
-export class RequestService {
-  apikey = environment.apiKey;
-  username: string;
-  private repo: string;
-  constructor(private http: HttpClient) {
-
-  }
-
-
-  reposLink(reposearch) {
-
-
-    return this.http.get<JSON[]>(`https://api.github.com/repos/` + reposearch)
-
-
-
-
-  }
+export class Users {
+}
