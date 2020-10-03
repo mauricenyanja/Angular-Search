85178e15ba38eaf0bbe5b6ff6352704da8230339
Nyash-Mauro <mauricee423@gmail.com>
2020-10-03 03:57:17 +0300

    added directives

--- src/app/users/users.ts
+++ src/app/users/users.ts
@@ -1,2 +1,23 @@
-export class Users {
-}
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable()
+export class RequestService {
+  apikey = environment.apiKey;
+  username: string;
+  private repo: string;
+  constructor(private http: HttpClient) {
+
+  }
+
+
+  reposLink(reposearch) {
+
+
+    return this.http.get<JSON[]>(`https://api.github.com/repos/` + reposearch)
+
+
+
+
+  }
---
+++ src/hover.directive.spec.ts
@@ -0,0 +1,7 @@
+import { Hover.Directive } from './hover.directive';
+
+describe('Hover.Directive', () => {
+  it('should create an instance', () => {
+    expect(new Hover.Directive()).toBeTruthy();
+  });
+});
